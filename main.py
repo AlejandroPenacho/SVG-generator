93635877c6144bbc3239b5c4c05bc2f8d6a300ad
Alejandro Penacho <alejandro.per@hotmail.es>
2021-07-24 21:05:23 +0200

    Working structure

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+#import pdb
+#pdb.set_trace()
+
 class SVGelement:
-	def __init__(self):
-		pass
+	def __init__(self, id):
+		self.id = id
+		self.children = []
+
+	def add_children(self, children):
+		self.children.append(children)
+
 
 class SVGgroup(SVGelement):
 	def __init__(self, id):
@@ -16,27 +24,31 @@ class SVGobject(SVGelement):
 		self.type = type
 
 
-def addElement(filename):
+def parse_svg(filename):
+	file = open(filename)
+	return add_element(file)
+
+def add_element(file):
 	# Gets the data and children of the next element in the file. Since the next
 	# line can contain the end of the current element (like </circle>), the
 	# output value has two elements : [endCurrentElement, element]. 
 
-	[type, element] = getElementData(filename)	
+	[type, element] = get_block(file)	
 	if (type == "endCurrentElement"):
 		return [True, 0]
-	else if (type == "noChildren"):
+	elif (type == "noChildren"):
 		return [False, element]
 	
 	while True:
-		[type, element] = addElement(filename)	
-		if (type == "endCurrentElement"):
+		[endThisElement, child] = add_element(file)	
+		if endThisElement:
 			return [False, element]
 		else:
-			element.addChildren(element)
+			element.add_children(child)
 		
 	
 
-def getElementData(filename):
+def get_block(file):
 	# Reads all information between two traingular brackets (<>). These contain
 	# an end of element, or the information regarding a new one. Depending on
 	# whether it ends with ">" or "/>", the next elements will be children of
@@ -44,9 +56,34 @@ def getElementData(filename):
 	# [type, element]. Type can be: "endElement", "noChildren" or "withChildren".
 	#
 	#	- "endCurrentElement": single line indicating the exit of the current element,
-	#	  					   </***>.
+	#		   </***>.
 	#	- "noChilden": an element with no children, so it can be added
-	#				   inmediately to the children of the parent element.
+	#	   inmediately to the children of the parent element.
 	#	- "withChidlren": element with children, so elements must be added to it
-	#					  as children until an "endCurrentElement" is read.
+	#		  as children until an "endCurrentElement" is read.
+
+	data_lines = [file.readline().strip()]
+	if (data_lines[0][0:2] == "</"):
+		return ["endCurrentElement", 0]
+
+	while True:
+		if data_lines[-1][-1] == ">":
+			break
+		else:
+			data_lines.append(file.readline().strip())
+
+	if data_lines[-1][-2:] == "/>":
+		childrenStatus = "noChildren"
+	else:
+		childrenStatus = "withChildren"
+
+	return [childrenStatus, process_block(data_lines)]
+
+
+def process_block(data_lines):
+	out = SVGelement("www")
+	out.data = data_lines
+	return out
+
+out = parse_svg("test/example.svg")
 
